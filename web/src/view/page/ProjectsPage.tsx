c7f78ff1946439bfa5fe430c2b54b50090daa99a
Chelsey Wang <chelseyyywang@gmail.com>
2020-10-21 20:36:56 -0700

    cleaned up + added state to form

--- web/src/view/page/ProjectsPage.tsx
+++ web/src/view/page/ProjectsPage.tsx
@@ -1,7 +1,6 @@
 import { RouteComponentProps } from '@reach/router'
 import * as React from 'react'
 import { style } from '../../style/styled'
-import { BodyText } from '../../style/text'
 import { AppRouteParams } from '../nav/route'
 import { Page } from './Page'
 
@@ -16,10 +15,20 @@ interface TestUser {
   location: string
 }
 
+interface Post {
+  name: string
+  price: number
+  start: string
+  end: string
+  location: string
+  tags: string
+  description: string
+}
+
 export function ProjectsPage(props: ProjectsPageProps) {
   const [editForm, showEditForm] = React.useState(false)
   const [user, editUser] = React.useState<TestUser>({
-    name: 'Julia B.',
+    name: 'Julia Baylon',
     email: 'julia@gmail.com',
     phone: '(123) 456 - 7890',
     location: 'Westwood, CA',
@@ -32,9 +41,9 @@ export function ProjectsPage(props: ProjectsPageProps) {
             <div
               style={{
                 margin: 'auto',
-                width: '50vh',
-                height: '50vh',
-                borderRadius: '50vh',
+                width: '180px',
+                height: '180px',
+                borderRadius: '180px',
                 border: '0.5px solid #18A0FB',
                 backgroundPositionY: 'center',
                 backgroundSize: 'cover',
@@ -48,12 +57,12 @@ export function ProjectsPage(props: ProjectsPageProps) {
           <div style={{ flex: 2, flexDirection: 'column' }}>
             {editForm ? (
               <form onSubmit={() => showEditForm(false)}>
-                <LabelText>Edit My Information: </LabelText>
+                <HeaderLabelText> EDIT MY INFORMATION: </HeaderLabelText>
                 <FormInput>
                   <input
                     type="text"
-                    placeholder="nameee"
-                    style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
+                    placeholder="name"
+                    style={{ fontSize: '0.9em', color: '#303030', resize: 'none', width: '100%' }}
                     onChange={e =>
                       editUser({ name: e.target.value, email: user.email, phone: user.phone, location: user.location })
                     }
@@ -64,7 +73,7 @@ export function ProjectsPage(props: ProjectsPageProps) {
                   <input
                     type="text"
                     placeholder="email"
-                    style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
+                    style={{ fontSize: '0.9em', color: '#303030', resize: 'none', width: '100%' }}
                     onChange={e =>
                       editUser({ name: user.name, email: e.target.value, phone: user.phone, location: user.location })
                     }
@@ -75,7 +84,7 @@ export function ProjectsPage(props: ProjectsPageProps) {
                   <input
                     type="text"
                     placeholder="phone number"
-                    style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
+                    style={{ fontSize: '0.9em', color: '#303030', resize: 'none', width: '100%' }}
                     onChange={e =>
                       editUser({ name: user.name, email: user.email, phone: e.target.value, location: user.location })
                     }
@@ -86,13 +95,15 @@ export function ProjectsPage(props: ProjectsPageProps) {
                   <input
                     type="text"
                     placeholder="location"
-                    style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
+                    style={{ fontSize: '0.9em', color: '#303030', resize: 'none', width: '100%' }}
                     onChange={e =>
                       editUser({ name: user.name, email: user.email, phone: user.phone, location: e.target.value })
                     }
                     value={user.location}
                   />
                 </FormInput>
+                <input accept="image" type="file" />
+                <br />
                 <SubmitButton type="submit">
                   <LabelText>SUBMIT</LabelText>
                 </SubmitButton>
@@ -109,19 +120,17 @@ export function ProjectsPage(props: ProjectsPageProps) {
         </Row>
         <NewPostForm />
       </div>
-      <MyAccount name={user.name} email={user.email} phone={user.phone} location={user.location} />
-      <BodyText>
+      <p style={{ color: 'white' }}>
         The goal of the course project is to gain hands-on experience building and deploying a scalable web service on
-        the internet. This is a "learn by doing" course so we'll spend time in lectures and lab sessions building
-        features, watching them fail at scale, and then fixing them. 😉
-      </BodyText>
+        the internet. This is a "learn
+      </p>
     </Page>
   )
 }
 function MyAccountInfo(props: TestUser) {
   return (
     <>
-      <LabelText>MY INFORMATION:</LabelText>
+      <HeaderLabelText> MY INFORMATION:</HeaderLabelText>
       <FormInput>
         <FormText>{props.name}</FormText>
       </FormInput>
@@ -137,183 +146,168 @@ function MyAccountInfo(props: TestUser) {
     </>
   )
 }
-// function MyAccountForm() {
-//   // const [user] = React.useState<TestUser>({ name: 'Julia B.', email: 'julia@gmail.com', phone: '(123) 456 - 7890' })
-
-//   return (
-//     <>
-//       <form>
-//         <h1>Fo rm</h1>
-//         <FormInput>
-//           <input
-//             type="text"
-//             placeholder="name"
-//             style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
-//             // value={user.name}
-//           />
-//         </FormInput>
-//         <FormInput>
-//           <input
-//             type="text"
-//             placeholder="email"
-//             style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
-//           />
-//         </FormInput>
-//         <FormInput>
-//           <input
-//             type="text"
-//             placeholder="phone number"
-//             style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
-//           />
-//         </FormInput>
-//         <FormInput>
-//           <input
-//             type="text"
-//             placeholder="location"
-//             style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
-//           />
-//         </FormInput>
-//         <SubmitButton type="submit">
-//           <LabelText>SUBMIT</LabelText>
-//         </SubmitButton>
-//       </form>
-//     </>
-//   )
-// }
 
 function NewPostForm() {
+  const [post, editPost] = React.useState<Post>({
+    name: '',
+    price: 0,
+    start: '',
+    end: '',
+    location: '',
+    tags: '',
+    description: '',
+  })
   return (
     <>
       <form>
-        <LabelText>NEW POST: </LabelText>
+        <HeaderLabelText>NEW POST: </HeaderLabelText>
+        <FormLabelText>service name </FormLabelText>
         <FormInput>
           <input
             type="text"
             placeholder="Service Name"
-            style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
+            style={{ fontSize: '0.9em', color: '#303030', resize: 'none', width: '100%' }}
+            onChange={e =>
+              editPost({
+                name: e.target.value,
+                price: post.price,
+                start: post.start,
+                end: post.end,
+                location: post.location,
+                tags: post.tags,
+                description: post.description,
+              })
+            }
           />
         </FormInput>
+        <FormLabelText>service price </FormLabelText>
         <FormInput>
           <input
             type="number"
             placeholder="Service Price"
-            style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
+            style={{ fontSize: '0.9em', color: '#303030', resize: 'none', width: '100%' }}
+            onChange={e =>
+              editPost({
+                name: post.name,
+                price: parseInt(e.target.value),
+                start: post.start,
+                end: post.end,
+                location: post.location,
+                tags: post.tags,
+                description: post.description,
+              })
+            }
           />
         </FormInput>
+        <FormLabelText>service availability start date</FormLabelText>
         <FormInput>
           <input
             type="date"
             placeholder="Service Availability"
-            style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
+            style={{ fontSize: '0.9em', color: '#303030', resize: 'none', width: '100%' }}
+            onChange={e =>
+              editPost({
+                name: post.name,
+                price: post.price,
+                start: e.target.value,
+                end: post.end,
+                location: post.location,
+                tags: post.tags,
+                description: post.description,
+              })
+            }
           />
         </FormInput>
+        <FormLabelText>service availability end date</FormLabelText>
+        <FormInput>
+          <input
+            type="date"
+            placeholder="Service Availability"
+            style={{ fontSize: '0.9em', color: '#303030', resize: 'none', width: '100%' }}
+            onChange={e =>
+              editPost({
+                name: post.name,
+                price: post.price,
+                start: post.start,
+                end: e.target.value,
+                location: post.location,
+                tags: post.tags,
+                description: post.description,
+              })
+            }
+          />
+        </FormInput>
+        <FormLabelText>service location </FormLabelText>
         <FormInput>
           <input
             type="text"
             placeholder="Service Location"
-            style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
+            style={{ fontSize: '0.9em', color: '#303030', resize: 'none', width: '100%' }}
+            onChange={e =>
+              editPost({
+                name: post.name,
+                price: post.price,
+                start: post.start,
+                end: post.end,
+                location: e.target.value,
+                tags: post.tags,
+                description: post.description,
+              })
+            }
           />
         </FormInput>
+        <FormLabelText> service tags </FormLabelText>
         <FormInput>
           <input
             type="text"
             placeholder="Service Tags"
-            style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
+            style={{ fontSize: '0.9em', color: '#303030', resize: 'none', width: '100%' }}
+            onChange={e =>
+              editPost({
+                name: post.name,
+                price: post.price,
+                start: post.start,
+                end: post.end,
+                location: post.location,
+                tags: e.target.value,
+                description: post.description,
+              })
+            }
           />
         </FormInput>
+        <FormLabelText>service description </FormLabelText>
         <FormInput>
           <input
             type="text"
             placeholder="Service Description"
-            style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
+            style={{ fontSize: '0.9em', color: '#303030', resize: 'none', width: '100%' }}
+            onChange={e =>
+              editPost({
+                name: post.name,
+                price: post.price,
+                start: post.start,
+                end: post.end,
+                location: post.location,
+                tags: post.tags,
+                description: e.target.value,
+              })
+            }
           />
         </FormInput>
-        <input
-          accept="image"
-          type="file"
-          // placeholder="Service Description"
-          // style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
-        />
+        <FormLabelText>upload image(s) </FormLabelText>
+        <input style={{ marginTop: '10px' }} accept="image" type="file" />
         <br />
         <SubmitButton type="submit">
           <LabelText>SUBMIT</LabelText>
         </SubmitButton>
       </form>
-    </>
-  )
-}
-function MyAccount(props: TestUser) {
-  const [editForm, showEditForm] = React.useState(true)
-  return (
-    <>
-      <div style={{ width: '100%' }}>
-        <Row>
-          <div style={{ flex: 1 }}>
-            <div
-              style={{
-                margin: 'auto',
-                width: '50vh',
-                height: '50vh',
-                borderRadius: '50vh',
-                border: '0.5px solid #18A0FB',
-                backgroundPositionY: 'center',
-                backgroundSize: 'cover',
-                backgroundImage:
-                  'url(' +
-                  'https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=670&q=80' +
-                  ')',
-              }}
-            ></div>
-          </div>
-          <div style={{ flex: 2, flexDirection: 'column' }}>
-            {editForm ? (
-              <form onSubmit={() => showEditForm(false)}>
-                <LabelText>Edit My Information: </LabelText>
-                <FormInput>
-                  <input
-                    type="text"
-                    placeholder="name"
-                    style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
-                    // value={user.name}
-                  />
-                </FormInput>
-                <FormInput>
-                  <input
-                    type="text"
-                    placeholder="email"
-                    style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
-                  />
-                </FormInput>
-                <FormInput>
-                  <input
-                    type="text"
-                    placeholder="phone number"
-                    style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
-                  />
-                </FormInput>
-                <FormInput>
-                  <input
-                    type="text"
-                    placeholder="location"
-                    style={{ fontSize: '0.9em', color: '#808080', resize: 'none', width: '100%' }}
-                  />
-                </FormInput>
-                <SubmitButton type="submit">
-                  <LabelText>SUBMIT</LabelText>
-                </SubmitButton>
-              </form>
-            ) : (
-              <>
-                <MyAccountInfo name={props.name} email={props.email} phone={props.phone} location={props.location} />
-                <SubmitButton onClick={() => showEditForm(true)} style={{ marginBottom: '16px' }}>
-                  <LabelText>EDIT</LabelText>
-                </SubmitButton>
-              </>
-            )}
-          </div>
-        </Row>
-        <NewPostForm />
-      </div>
+      <h2>{post.name}</h2>
+      <h2>{post.price}</h2>
+      <h2>{post.start} </h2>
+      <h2>{post.end} </h2>
+      <h2>{post.tags} </h2>
+      <h2>{post.location} </h2>
+      <h2>{post.description}</h2>
     </>
   )
 }
@@ -326,6 +320,7 @@ const FormInput = style('div', {
   padding: '5px',
   paddingLeft: '10px',
   margin: '5px',
+  minHeight: '13px',
 })
 const SubmitButton = style('button', {
   display: 'block',
@@ -338,5 +333,7 @@ const SubmitButton = style('button', {
   marginLeft: 'auto',
   marginRight: 'auto',
 })
+const FormLabelText = style('h1', { fontSize: '0.9em', letterSpacing: '1.25px', marginLeft: '8px' })
 const LabelText = style('h1', { fontSize: '0.9em', letterSpacing: '1.25px' })
+const HeaderLabelText = style('h1', { fontSize: '1.2em', letterSpacing: '1.25px' })
 const FormText = style('p', { fontSize: '0.9em', color: 'black', resize: 'none', width: '100%' })
